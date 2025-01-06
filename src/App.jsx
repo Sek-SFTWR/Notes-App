02b782c9078a81f9ea44498786cec2c97cf0515e
DDAM-M0084 <ddam-m0084@DDAM-M0084.local>
2025-01-06 11:17:07 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import reactLogo from "./assets/react.svg";
+import React from "react";
 import "./App.css";
-
+console.log(React);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
+        <h1 className="header">Hello from Amplify</h1>
+        hi
       </header>
     </div>
   );
